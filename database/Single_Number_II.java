bd2422bb586907dc00fe7347a2e448c22cf44612
jiangchuan02@baidu.com <jiangchuan02@baidu.com>
2018-07-09 17:02:42 +0800

    Single Number II

---
+++ database/Single_Number_II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        for (Integer key : map.keySet()) {
+            if (map.get(key) == 1) {
+                return key;
+            }
+        }
+        return 0;
+    }
+}
