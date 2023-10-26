2fbeee86a4bbe2770588e3f29047950be1913e0d
Jeffrey Jiang <jeffreyjiang@JeffreydeMac-Studio.local>
2023-10-27 00:34:40 +0800

    20231027

---
+++ FirstBadVersion.go
@@ -0,0 +1,26 @@
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+	fst := 0
+	end := n
+	for fst < end {
+		mid := (end + fst) / 2
+		if isBadVersion(mid) {
+			// 如果是bad v，则表示左边可能存在，向左找
+			end = mid
+		} else {
+			// 如果不是，则表示在右边
+			fst = mid + 1
+		}
+	}
+	if isBadVersion(fst) {
+		return fst
+	}
+	return 0
+}
